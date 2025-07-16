43253e91c8273f30e208bfe46282e642becf9682
unknown <joannakolbe@wp.pl>
2025-07-16 15:43:09 +0200

    Add random move logic for computerMove

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+function printMessage(msg){
+  var div = document.createElement('div');
+  div.innerHTML = msg;
+  document.getElementById('messages').appendChild(div);
+}
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Twój ruch to ' + playerMove + '. Jeśli mój ruch to kamień, to wygrywasz!');
+function clearMessages(){
+  document.getElementById('messages').innerHTML = '';
+}
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+
+printMessage('Mój ruch: ' + computerMove);
