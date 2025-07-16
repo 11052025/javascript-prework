b8e862719887a830385322625d9eca6ae2562e11
unknown <joannakolbe@wp.pl>
2025-07-16 10:08:50 +0200

    Add initial moves variables and messages in script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Twój ruch to ' + playerMove + '. Jeśli mój ruch to kamień, to wygrywasz!');
